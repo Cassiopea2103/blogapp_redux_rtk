3de3acd909c879c6fe8c888fa1c60242b263e85b
unknown <wadess@ept.sn>
2024-08-28 10:16:07 +0000

    users slice for fetching users data

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,3 +1,60 @@
 import { createEntityAdapter , createSelector } from "@reduxjs/toolkit";
+import { apiSlice } from '../api/apiSlice' ; 
 
 // users adapter : 
+const usersAdapter = createEntityAdapter ({}) ; 
+
+// initial state : 
+const initialState = usersAdapter.getInitialState ({}) ;
+
+// injected endpoints to apiSlice : 
+const usersSlice = apiSlice.injectEndpoints (
+    {
+        endpoints : ( builder ) => (
+            {
+                // fetch users : 
+                fetchUsers : builder.query (
+                    {
+                        query : () => (
+                            {
+                                url : '/users' , 
+                                method : 'GET'
+                            }
+                        ) , 
+                        transformREsponse : responseData => {
+                            responseData.map (
+                                user => {
+                                    // convert its id to number : 
+                                    user.id = Number ( user.id ) ; 
+
+                                    return user ; 
+                                }
+                            )
+
+                            // set initial state to fetched users data : 
+                            return usersAdapter.setAll ( initialState , responseData ) ; 
+                        }
+                    }
+                )
+            }
+        )
+    }
+)
+
+// slice hook : 
+export const { useFetchUsersQuery } = usersSlice ; 
+
+// fetch users query result : 
+const queryResult = usersSlice.endpoints.fetchUsers.select() ; 
+
+// memoized fetch users data selector : 
+const selectUsersData = createSelector (
+    queryResult => queryResult.data 
+)
+
+// users adapter selectors based on memoized data : 
+export const {
+    selectAll : selectAllUsers , 
+    selectById : selectUserById , 
+    selectIds : selectUsersIds 
+} = usersAdapter.getSelectors ( state => selectUsersData ( state ) ?? initialState ) ; 
